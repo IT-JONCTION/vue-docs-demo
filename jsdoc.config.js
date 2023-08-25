87b8707c6cbb7c92e5ad1d9e2e28324c15eb6201
waynetheisinger <wayne@mowdirect.co.uk>
2023-08-25 11:05:24 +0100

    new theme

--- jsdoc.config.js
+++ jsdoc.config.js
@@ -1,13 +1,29 @@
 module.exports = {
   plugins: [
-    "node_modules/jsdoc-vuejs"
+    "node_modules/jsdoc-vuejs",
+    "plugins/markdown"
   ],
+  tags: {
+    "allowUnknownTags": true,
+    "dictionaries": ["jsdoc"]
+  },
   recurseDepth: 10,
   source: {
-    include: ["src"],
-    includePattern: ".*\\.(js|jsx|vue)$"
+    include: ["./src", "package.json", "README.md"],
+    includePattern: ".(js|jsx|vue)$",
+    excludePattern: "(node_modules|./docs)"
+  },
+  templates: {
+    cleverLinks: false,
+    monospaceLinks: true,
+    useLongnameInNav: false,
+    showInheritedInNav: true
   },
   opts: {
-    destination: "./docs/"
+    destination: "./docs/",
+    encoding: "utf8",
+    private: true,
+    recurse: true,
+    template: "./node_modules/minami"
   }
 };
